4de10604eddb3af85a8c96f773d8b1b45afa36d0
DONAT: Developers of Next Advanced Technology <152084590+DonatOrca@users.noreply.github.com>
2024-02-01 13:49:35 +0800

    change: phone navigation

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./DarkMode.css";
 
 const DarkMode = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
   const setDarkMode = () => {
     document.querySelector("body")?.setAttribute("data-theme", "dark");
     localStorage.setItem("selectedTheme", "dark");
   };
+
   const setLightMode = () => {
     document.querySelector("body")?.setAttribute("data-theme", "light");
     localStorage.setItem("selectedTheme", "light");
   };
+
   const toggleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.checked) setDarkMode();
+    const checked = e.target.checked;
+    setIsChecked(checked);
+
+    if (checked) setDarkMode();
     else setLightMode();
   };
 
-  const selectedTheme = localStorage.getItem("selectedTheme");
-  if (selectedTheme === "dark") setDarkMode();
+  useEffect(() => {
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    setIsChecked(prefersDarkMode);
+
+    if (prefersDarkMode) {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
+  }, []);
 
   return (
     <div className="dark_mode">
@@ -25,7 +42,7 @@ const DarkMode = () => {
         type="checkbox"
         id="darkmode-toggle"
         onChange={toggleTheme}
-        defaultChecked={selectedTheme === "dark"}
+        checked={isChecked}
       />
       <label className="dark_mode_label" htmlFor="darkmode-toggle"></label>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,58 @@
 import { Link } from "react-router-dom";
-import dropDownMenu from "../assets/svg/DropDownMenu.svg";
 import DarkMode from "./DarkMode";
 import "./Navbar.css";
 import "../Fonts.css";
+import useMediaQuery from "./useMediaQuery";
 
 function Navbar() {
+  const matches = useMediaQuery("(min-width: 992px)");
+
   return (
-    <>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        {matches ? "" : <DarkMode />}
+        <button
+          className="navbar-toggler justify-content-end"
+          itemType="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            fill="currentColor"
+            className="bi bi-list"
+            viewBox="0 0 16 16"
           >
-            <img src={dropDownMenu} alt="dropdown menu" />
-          </button>
-          <div
-            className="collapse navbar-collapse justify-content-end"
-            id="navbarNavAltMarkup"
-          >
-            <div className="navbar-nav">
-              <Link to="/DonatOrca/" className="nav-link">
-                Dashboard
-              </Link>
-              <Link to="/DonatOrca/announcement" className="nav-link">
-                Announcement
-              </Link>
-              <Link to="/DonatOrca/officers" className="nav-link">
-                Officers
-              </Link>
-              <Link to="/DonatOrca/history" className="nav-link">
-                History
-              </Link>
-              <DarkMode />
-            </div>
+            <path d="M2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zM2 7.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zM2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+          </svg>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav">
+            <Link to="/DonatOrca/" className="nav-link">
+              Dashboard
+            </Link>
+            <Link to="/DonatOrca/announcement" className="nav-link">
+              Announcement
+            </Link>
+            <Link to="/DonatOrca/officers" className="nav-link">
+              Officers
+            </Link>
+            <Link to="/DonatOrca/history" className="nav-link">
+              History
+            </Link>
+            {matches ? <DarkMode /> : ""}
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
 
