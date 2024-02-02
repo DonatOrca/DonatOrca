1f8278db408d9071a0836dd1d96d4c7d91a616fd
audrNa <audrna7@gmail.com>
2024-02-02 14:35:50 +0800

    Finalize Constitution styling

--- src/pages/Constitution.tsx
+++ src/pages/Constitution.tsx
@@ -1,4 +1,4 @@
-import '../css/Constitution.css';
+import "../css/Constitution.css";
 
 const Constitution = () => {
   return (
@@ -6,22 +6,122 @@ const Constitution = () => {
       <h1>BYLAWS AND CONSTITUTION</h1>
 
       <h2>PREAMBLE</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus nihil neque, molestiae eius eveniet possimus ex nulla, maiores aliquid numquam quaerat culpa? Asperiores, beatae qui! Sequi repellendus asperiores laboriosam corporis. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo impedit consequatur ratione minus ea sequi ipsum aspernatur ex ipsam. Fugiat voluptatum distinctio veniam temporibus nisi culpa enim numquam place</p>
+      <div>
+        <p id="preamble">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          nihil neque, molestiae eius eveniet possimus ex nulla, maiores aliquid
+          numquam quaerat culpa? Asperiores, beatae qui! Sequi repellendus
+          asperiores laboriosam corporis. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Explicabo impedit consequatur ratione
+          minus ea sequi ipsum aspernatur ex ipsam. Fugiat voluptatum distinctio
+          veniam temporibus nisi culpa enim numquam place
+        </p>
+      </div>
 
       <h2>ARTICLE I: GENERAL PROVISIONS</h2>
-      <p>Section 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deleniti illo aspernatur nulla exercitationem assumenda nemo sit facere, vero voluptate natus placeat at qui excepturi libero quis iusto non! Dignissimos.</p>
+      <div>
+        <p>
+          <strong>Section 1.</strong> Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+          exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+          at qui excepturi libero quis iusto non! Dignissimos.
+        </p>
+      </div>
 
       <h2>ARTICLE II: OFFICIAL NAME AND LOGO </h2>
-      <p>Section 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deleniti illo aspernatur nulla exercitationem assumenda nemo sit facere, vero voluptate natus placeat at qui excepturi libero quis iusto non! Dignissimos.</p>
-      <p>Section 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deleniti illo aspernatur nulla exercitationem assumenda nemo sit facere, vero voluptate natus placeat at qui excepturi libero quis iusto non! Dignissimos.</p>
-      <p>Section 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deleniti illo aspernatur nulla exercitationem assumenda nemo sit facere, vero voluptate natus placeat at qui excepturi libero quis iusto non! Dignissimos.</p>
-      <p>Section 4. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deleniti illo aspernatur nulla exercitationem assumenda nemo sit facere, vero voluptate natus placeat at qui excepturi libero quis iusto non! Dignissimos.</p>
+      <p>
+        <strong>Section 1.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+        exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+        at qui excepturi libero quis iusto non! Dignissimos.
+      </p>
+      <p>
+        <strong>Section 2.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+        exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+        at qui excepturi libero quis iusto non! Dignissimos.
+      </p>
+      <p>
+        <strong>Section 3.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+        exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+        at qui excepturi libero quis iusto non! Dignissimos.
+      </p>
+      <p>
+        <strong>Section 4.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+        exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+        at qui excepturi libero quis iusto non! Dignissimos.
+      </p>
 
       <h2>ARTICLE III: DECLARATION OF PRINCIPLES AND OBJECTIVES </h2>
-      <p>Section 1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deleniti illo aspernatur nulla exercitationem assumenda nemo sit facere, vero voluptate natus placeat at qui excepturi libero quis iusto non! Dignissimos.</p>
-      <p>Section 2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, deleniti illo aspernatur nulla exercitationem assumenda nemo sit facere, vero voluptate natus placeat at qui excepturi libero quis iusto non! Dignissimos.</p>
+      <p>
+        <strong>Section 1.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+        exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+        at qui excepturi libero quis iusto non! Dignissimos.
+      </p>
+      <p>
+        <strong>Section 2.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+        exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+        at qui excepturi libero quis iusto non! Dignissimos.
+      </p>
+
+      <h2>ARTICLE IV: MEMBERSHIP</h2>
+      <p>
+        <strong>Section 1.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium, deleniti illo aspernatur nulla
+        exercitationem assumenda nemo sit facere, vero voluptate natus placeat
+        at qui excepturi libero quis iusto non! Dignissimos.
+      </p>
+      <p>
+        <strong>Section 2.</strong> Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Praesentium.
+        <ul>
+          <li>
+            <strong>2.1.</strong> lorem ipsum bla batender bak baka shine.
+          </li>
+          <li>
+            <strong>2.1.</strong> lorem ipsum bla batender bak baka shine.
+          </li>
+          <li>
+            <strong>2.1.</strong> lorem ipsum bla batender bak baka shine.
+          </li>
+        </ul>
+      </p>
+
+      <h2>ARTICLE V: BILL OF RIGHTS</h2>
+      <p>
+        <strong>Section 1.</strong> Mondaishuu de kitaa Shinkansen gokkou
+        Suttokoton suttokoton Suttoko ton ton toon Nyan! Nyanpasu!! (Hai)
+      </p>
+      <p>
+        <strong>Section 2.</strong> Yabure kabure no yabuisha ga Takeyabu no
+        naka e sutta kora sa.
+      </p>
+      <p>
+        <strong>Section 3.</strong> Yabure kabure no yabuisha ga Takeyabu no
+        naka e sutta kora sa.
+      </p>
+      <p>
+        <strong>Section 4.</strong> Yabure kabure no yabuisha ga Takeyabu no
+        naka e sutta kora sa.
+      </p>
+      <p>
+        <strong>Section 5.</strong> Yabure kabure no yabuisha ga Takeyabu no
+        naka e sutta kora sa.
+      </p>
+      <p>
+        <strong>Section 6.</strong> Yabure kabure no yabuisha ga Takeyabu no
+        naka e sutta kora sa.
+      </p>
+      <p>
+        <strong>Section 7.</strong> Yabure kabure no yabuisha ga Takeyabu no
+        naka e sutta kora sa.
+      </p>
     </div>
   );
-}
+};
 
 export default Constitution;
