8d43a3ec00481e0dfa99dcab0b25102a5f9ac19d
audrNa <audrna7@gmail.com>
2024-02-11 16:41:12 +0800

    Implement Constitution in Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,26 @@
+import { useOutletContext } from "react-router-dom";
+
+import Bylaws from "../components/Constitution";
+
 function Dashboard() {
-  return <>test</>;
+  const [setModal, setModalVisible] = useOutletContext();
+
+  return (
+    <div className="m-4">
+      <button
+        className="rounded-md bg-txc p-6 font-bebas text-4xl text-bgc"
+        onClick={() => {
+          setModal({
+            header: "CONSTITUTION AND BY-LAWS",
+            children: <Bylaws />,
+          });
+          setModalVisible(true);
+        }}
+      >
+        Bylaws and Constitution
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
